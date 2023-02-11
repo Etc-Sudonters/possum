3de28c62be9dc2f860a06f02a6623786f0e2896c
ɹuɐ <etc@sudonters.com>
2023-02-11 17:53:48 -0500

    Okay, this actually looks like real engineering now

--- src/scavenge/workflow/parser.rs
+++ src/scavenge/workflow/parser.rs
@@ -1,38 +1,59 @@
 use std::marker::PhantomData;
 
+use yaml_peg::parser::{Loader, PError};
 use yaml_peg::repr::Repr;
+use yaml_peg::{Map,Yaml};
 
+use crate::document::DocumentPointer;
 use crate::document::{Annotation, Annotations};
 
-use super::ast;
-use super::ast::{job, on};
-
 pub enum ParseFailure {
-    TooManyDocuments(u8),
+    InvalidDocument(PError),
+    Empty,
+    NotAWorkflow,
+    TooManyDocuments(Vec<DocumentPointer>),
+    NotAMap(String),
 }
 
-pub struct WorkflowParser<'a, R>
+pub fn parse<'a, R>(l: &'a Loader<'a, R>, a: &'a mut Annotations) -> Result<(), ParseFailure>
 where
     R: Repr,
 {
-    _x: &'a PhantomData<R>,
-    annotations: Annotations,
+    let documents = l.parse().map_err(|e| ParseFailure::InvalidDocument(e))?;
+    match documents[..] {
+        [] => Err(ParseFailure::Empty),
+        [n] => {
+            match &'a n.yaml() {
+                Yaml::Map(m) => Ok(WorkflowParser::new(a).parse(m)),
+                _ => Err(ParseFailure::NotAMap("Root yaml node must be an object".to_owned())),
+                 
+            }
+        }
+        _ => Err(ParseFailure::TooManyDocuments(vec![])),
+    }
+}
+
+struct WorkflowParser<'a, R>
+where
+    R: Repr,
+{
+    _x: PhantomData<R>,
+    annotations: &'a mut Annotations,
 }
 
 impl<'a, R> WorkflowParser<'a, R>
 where
     R: Repr,
 {
-    pub fn parse(&mut self, n: &'a yaml_peg::Map<R>) {
-        for (k, v) in n.iter() {}
+    pub fn new(a: &'a mut Annotations) -> WorkflowParser<'a, R> {
+        WorkflowParser {
+            annotations: a,
+            _x: PhantomData,
+        }
     }
+    pub fn parse(mut self, l: &'a yaml_peg::Map<R>) -> () {}
 
-    fn annotation(&mut self, a: Annotation) {
+    fn annotate(&mut self, a: Annotation) {
         self.annotations.add(a)
     }
-
-    fn visit_name(&mut self, n: &'a ast::Node<String>) {}
-    fn visit_run_name(&mut self, n: &'a ast::Node<String>) {}
-    fn visit_on(&mut self, n: &'a ast::Node<on::Trigger>) {}
-    fn visit_job(&mut self, n: &'a ast::Node<job::Job>) {}
 }
